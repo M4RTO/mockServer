64dc696b274a8abdcb98ca2a3a634ab7fd323036
chano <martin.britez@gmail.com>
2021-02-02 12:25:28 -0300

    first commit

---
+++ src/prueba.ts
@@ -0,0 +1,17 @@
+import { expect } from "./mockserver";
+
+export class Prueba {
+
+    configMockServer() {
+
+        expect("GET", "/foo", 201, {
+            "foo": "bar"
+        },0);
+
+
+    }
+}
+
+
+
+
---
+++ test/all.test.ts
@@ -0,0 +1,10 @@
+
+import { Prueba } from "../src/prueba";
+describe('Decidir', () => {
+  //reset()
+
+   it('config mockserver for Prueba', () => {
+     new Prueba().configMockServer()
+   });
+
+});
